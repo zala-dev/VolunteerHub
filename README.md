a9a5c5e29292634b96d149c7261ec7264699b802
John Drabble <jrdrabble@gmail.com>
2024-06-27 10:25:29 +1000

    refactored views and added comments to help others working on the code

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # VolunteerHub
 
 ## User Stories
-An event organising app for volunteer events. Users can join and/or create and organise a volunteering event. 
 
-As a user, 
+An event organising app for volunteer events. Users can join and/or create and organise a volunteering event.
+
+As a user,
+
 - I want to be able to sign up, log in and out of the application.
 - Only logged in users to be able to see volunteering event information.
 - I want to be able to volunteer for an event and withdraw as a volunteer from an event.
 - I want to be able to like and unlike a volunteering event.
 - I want to be able to easily see all the events I have volunteered for.
--  I want to be able to see all the volunteering events that have been created, so I can volunteer for events that appeal to me.
+- I want to be able to see all the volunteering events that have been created, so I can volunteer for events that appeal to me.
 - I want all lists of events to be displayed in date and time order.
 - I want to be able to see how many people have volunteered for an event and the names of the volunteers.
 - I want to be able to see the total number of people that have liked a volunteering event.
@@ -18,20 +20,25 @@ As a user,
 - I want to be able to easily see all the events I have created/organised.
 
 ## URL
+
 (Add link)
 
 ## App image
-![log-in screen](photos-read-me/Login.png)
-![Image of event list](photos-read-me/EventList.png)
-![Image of creating an event](photos-read-me/CreateEvent.png)
+
+![log-in screen]()
+![Image of event list]()
+![Image of creating an event]()
 
 ## Wireframe
+
 (Needs work)
 
 ## Entity relationship diagram
+
 (Show ERD, Add link)
 
 ## Technologies Used
+
 - Python
 - Django
 - CSS
@@ -40,10 +47,13 @@ As a user,
 - Trello board
 
 ## MVP
+
 - (Discuss with team)
 
 ## Ice box
+
 As a user,
+
 - I want to receive notifications and reminders about volunteering events.
 - I want to be able to connect with other volunteers using the application.
 - I want to be able to provide comments on events.
@@ -51,7 +61,9 @@ As a user,
 - I want to post photos from the volunteer events I attended.
 
 ## Pitch deck
-(VolunteerHub - GA Unit 3 Project) * Add link
+
+(VolunteerHub - GA Unit 3 Project) \* Add link
 
 ## Trello board
+
 https://trello.com/b/7MWjfIRH/sprint-retrospective-template-trello
--- main_app/views.py
+++ main_app/views.py
@@ -7,91 +7,105 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+# render the home page, no authentication or authoristation is needed for this page.  All other views required user to be logged in. 
 def home(request):
   return render(request, 'home.html')
 
+#return all the events the logged in user has created / is the event organizer for. 
 class EventListView(LoginRequiredMixin, ListView):
     model = VolunteeringEvent
     template_name = 'event_list.html'
     context_object_name = 'events'
 
     def get_queryset(self):
-        # Get the events created by the logged-in user and order them by date and time 
+        # Get the events created by the logged in user and order them by date and time. 
         return VolunteeringEvent.objects.filter(organizer=self.request.user).order_by('date', 'time')
-    
+
+#return the details for a specific event the logged in user has created / is the event organizer for.     
 class EventDetailView(LoginRequiredMixin, DetailView):
     model = VolunteeringEvent
     template_name = 'event_detail.html'
     context_object_name = 'event'
 
-    #Raise a 403 no permission error if the event was not created by the logged-in user 
+    #Raise a 403 no permission error if the event was not created by the logged in user. 
     def get_object(self, queryset=None):
         event = super().get_object(queryset)
         if self.request.user != event.organizer:
             return self.handle_no_permission()
         return event
 
+# enable an event to be created by a logged in user. 
 class EventCreateView(LoginRequiredMixin, CreateView):
     model = VolunteeringEvent
     template_name = 'event_form.html'
     fields = ['title', 'description', 'date', 'time', 'location', 'volunteers_needed']
     success_url = reverse_lazy('event_list')
 
+# make sure the form is valid and add the logged in user id to the data. 
     def form_valid(self, form):
         form.instance.organizer = self.request.user
         return super().form_valid(form)
 
+# enable a specific event to be updated only by the user that created the event. 
 class EventUpdateView(LoginRequiredMixin, UpdateView):
     model = VolunteeringEvent
     template_name = 'event_form.html'
     fields = ['title', 'description', 'date', 'time', 'location', 'volunteers_needed']
     success_url = reverse_lazy('event_list')
 
+# if the event has not be created by the logged in user return a 403 error. 
     def dispatch(self, request, *args, **kwargs):
         event = self.get_object()
         if event.organizer != self.request.user:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
+# enable a specific event to be deleted only by the user that created the event. 
 class EventDeleteView(LoginRequiredMixin, DeleteView):
     model = VolunteeringEvent
     template_name = 'event_confirm_delete.html'
     success_url = reverse_lazy('event_list')
 
+# if the event has not be created by the logged in user return a 403 error.
     def dispatch(self, request, *args, **kwargs):
         event = self.get_object()
         if event.organizer != self.request.user:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
+# show all the events the logged in user has volunteered for, not limted to the events the logged in user has created. 
 class MyVolunteeringEventsListView(LoginRequiredMixin, ListView):
     model = VolunteeringEvent
     template_name = 'my_volunteering_events.html'
     context_object_name = 'my_events'
-    ordering = ['date', 'time']
 
+# order data by date and time
     def get_queryset(self):
-        event = self.request.user.volunteering_events.all()
-        event = event.order_by(*self.ordering)
-        return event
+        my_events = self.request.user.volunteering_events.all()
+        my_events = my_events.order_by('date', 'time')
+        return my_events
 
+# show all the volunteering events in the application, not limited to the events the logged in user has created or volunteered for.
 class AllVolunteeringEventsListView(LoginRequiredMixin, ListView):
     model = VolunteeringEvent
     template_name = 'all_events.html'
     context_object_name = 'events'
     ordering = ['date', 'time']
 
+# get the events the logged in user has liked, not limited to just the events they have created. 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         context['liked_events'] = set(user.like_set.values_list('event_id', flat=True)) if user.is_authenticated else set()
         return context
 
+# show the details of a specific volunteering event, not limited to the events the logged in user has created or volunteered for.
 class VolunteeringEventDetailView(LoginRequiredMixin, DetailView):
     model = VolunteeringEvent
     template_name = 'volunteering_event_detail.html'
     context_object_name = 'event'
-        
+
+# method for logged in user to volunteer for an event. Redirect back to the current page. 
 @login_required
 def add_volunteer(request, event_id):
     event = get_object_or_404(VolunteeringEvent, id=event_id)
@@ -99,25 +113,29 @@ def add_volunteer(request, event_id):
         event.volunteers.add(request.user)
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
+# method for logged in user to withdraw as a volunteer for an event they previously volunteered for. Redirect back to the current page. 
 @login_required
 def withdraw_volunteer(request, event_id):
     event = get_object_or_404(VolunteeringEvent, id=event_id)
     if request.user in event.volunteers.all():
         event.volunteers.remove(request.user)
     return redirect(request.META.get('HTTP_REFERER', '/'))
-      
+
+# method for logged in user to like an event. Redirect back to the current page. 
 @login_required
 def like_event(request, event_id):
-    event = get_object_or_404(VolunteeringEvent, pk=event_id)
+    event = get_object_or_404(VolunteeringEvent, id=event_id)
     Like.objects.get_or_create(user=request.user, event=event)
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
+# method for logged in user to unlike an event they previously liked. Redirect back to the current page. 
 @login_required
 def unlike_event(request, event_id):
-    event = get_object_or_404(VolunteeringEvent, pk=event_id)
+    event = get_object_or_404(VolunteeringEvent, id=event_id)
     Like.objects.filter(user=request.user, event=event).delete()
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
+# signup to use the application. Successful sign up will redirect to the list of all volunteering events. 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
